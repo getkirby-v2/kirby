1f764223238bc78169f6fd42dbe83244f7954665
Bastian Allgeier <mail@bastianallgeier.com>
2014-10-04 12:40:24 +0200

    updated system.php

--- system.php
+++ system.php
@@ -1,7 +1,21 @@
 <?php
 
-// load the bootstrapper
-include(__DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php');
+/**
+ * This is a legacy file, which makes it possible
+ * to keep using the old v1 index.php with v2.
+ * It's recommended to use the new index.php though.
+ */
 
-// start the cms
-echo kirby::start();
+define('DS', DIRECTORY_SEPARATOR);
+
+// load the bootstrapper
+include(__DIR__ . DS . 'bootstrap.php');
+
+// take the old variables to setup roots
+$kirby = kirby();
+$kirby->roots->index   = $root;
+$kirby->roots->site    = $rootSite;
+$kirby->roots->content = $rootContent;
+
+// render
+echo $kirby->launch();
