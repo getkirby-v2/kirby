5457c70cca7cc894c2e3bccd03f928d494bfc2bf
Bastian Allgeier <mail@bastianallgeier.com>
2015-01-14 11:44:30 +0100

    Kirbytext Tests

---
+++ test/KirbytextTest.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once('lib/bootstrap.php');
+
+class KirbytextTest extends PHPUnit_Framework_TestCase {
+
+  private function kt($value, $markdownExtra = false) {
+    $kirby = new Kirby(array(
+      'markdown.extra' => $markdownExtra
+    ));    
+    $kirby->roots->content = TEST_ROOT_ETC . DS . 'content';
+      
+    $site  = new Site($kirby);
+    $page  = new Page($site, '1-a');
+    $field = new Field($page, null, $value);
+
+    return new Kirbytext($field);
+
+  }
+
+  private function runTests($result) {
+
+    $root = TEST_ROOT_ETC . DS . 'kirbytext';
+    $dirs = dir::read($root);
+
+    foreach($dirs as $dir) {
+
+      $testFile     = $root . DS . $dir . DS . 'test.txt';
+      $expectedFile = $root . DS . $dir . DS . 'expected.html';
+
+      $this->assertEquals($result(f::read($testFile)), f::read($expectedFile));
+
+    }
+
+  }
+
+  public function testWithMarkdown() {
+
+    $self = $this;
+    $this->runTests(function($value) use($self) {
+      return (string)$self->kt($value);
+    });
+
+  }
+
+  public function testWithMarkdownExtra() {
+
+    $self = $this;
+    $this->runTests(function($value) use($self) {
+      return (string)$self->kt($value, true);
+    });
+
+  }
+
+}
