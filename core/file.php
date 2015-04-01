2d7212ddf22554be5f105d11d094d66fb1c99982
Pereira Ricardo <hello@nunopress.com>
2015-04-01 14:03:15 +0200

    Add thumb object to file image object directly.
    I think the best way for following the jQuery style is add the thumbnail object directly to file image object without need to calling with a simple function. Tested and working fine.

--- core/file.php
+++ core/file.php
@@ -214,6 +214,21 @@ abstract class FileAbstract extends Media {
     return true;
 
   }
+  
+  /**
+   * Add thumb object with jQuery style.
+   * 
+   * @param array $params
+   * @return null|Thumb
+   */
+  public function thumb($params = [])
+  {
+    if ('image' === $this->type()) {
+      return new Thumb($this, $params);
+    } else {
+      return null;
+    }
+  }
 
   /**
    * Converts the entire file object into 
