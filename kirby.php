c5d2e348b5d6f2fc34e5f666372f29098e799428
Bastian Allgeier <mail@bastianallgeier.com>
2015-11-18 11:08:47 +0100

    Updated the version number

--- kirby.php
+++ kirby.php
@@ -6,7 +6,7 @@ use Kirby\Request;
 
 class Kirby extends Obj {
 
-  static public $version = '2.2.0';
+  static public $version = '2.2.1';
   static public $instance;
   static public $hooks = array();
 
