d04e511aae24a53bd71685005044edd575328611
Nico Hoffmann <services@nhoffmann.com>
2016-02-29 21:58:05 +0100

    Modules: fixed fallback and cleaner code

--- kirby/modules.php
+++ kirby/modules.php
@@ -9,61 +9,55 @@ use Response;
 
 class Modules {
 
-  public $modules;
+  // core
+  public $controllers = array();
+  public $languages   = array();
+  public $models      = array();
+  public $snippets    = array();
+  public $tags        = array();
+  public $templates   = array();
+  public $autocss     = array();
+  public $autojs      = array();
 
-  public function __construct() {
-    $this->modules = array(
-      // core
-      'controllers' => array(),
-      'languages'   => array(),
-      'models'      => array(),
-      'snippets'    => array(),
-      'tags'        => array(),
-      'templates'   => array(),
-      'autocss'     => array(),
-      'autojs'      => array(),
+  // panel
+  public $blueprints  = array();
+  public $fields      = array();
 
-      // panel
-      'blueprints'  => array(),
-      'fields'      => array(),
-    );
+
+  /**
+   * Magic getter for module arrays
+   */
+  public function __call($method, $arguments) {
+    return isset($this->{$method}) ? $this->{$method} : null;
   }
 
   /**
    * Registers a plugin directory for a module type
-   * @param  [string] $type module type
-   * @param  [string] $dir  directory to be included as source
    */
-  public function register($type, $dir) {
-    if(!isset($this->modules[$type])) return false;
+  public function register($module, $dir) {
+    if(!isset($this->{$module})) return false;
 
     // prepare for auto-assets
-    if(in_array($type, array('autocss', 'autojs'))) {
+    if($module === 'autocss' or $module === 'autojs') {
       $route = 'assets/modules/' . sha1($dir);
       $dir   = array($dir, $route);
       $this->assets($route, $dir);
     }
 
-    array_push($this->modules[$type], $dir);
-  }
-
-  public function assets($route, $dir) {
-    return $this->route($route, $dir);
+    array_push($this->{$module}, $dir);
   }
 
   /**
    * Creates route for plugin assets
-   * @param  [string] $dir   plugin directory with assets
-   * @param  [type] $route   route to point to $dir
    */
-  public function route($route, $dir) {
+  public function route($route, $root) {
     if($page = page($route)) return false;
 
     return kirby()->routes(array(
       array(
         'pattern' => trim($route, '/') . '/(:all)',
-        'action'  => function($file) use($dir, $route) {
-          $path = $dir . DS . $file;
+        'action'  => function($file) use($route, $root) {
+          $path = $root . DS . $file;
           if(is_file($path)) {
             $extension = substr(strchr($path, '.'), 1);
             return new Response(f::read($path), $extension);
@@ -75,70 +69,63 @@ class Modules {
     ));
   }
 
-  public function __call($method, $arguments) {
-    return a::get($this->modules, $method, null);
+  public function assets($route, $root) {
+    return $this->route($route, $root);
   }
 
 
-  public function getAsset($type, $file) {
+  public function getAsset($module, $file) {
 
-    if($dirs = $this->{$type}()) {
+    if($dirs = $this->{$module}()) {
       foreach($dirs as $dir) {
         $root = $dir[0] . DS . $file;
         $url  = $dir[1] . DS . $file;
         if(f::exists($root)) return $url;
       }
-
-    } else {
-      return null;
     }
+
+    return null;
 
   }
 
-  public function getFile($type, $file, $extensions, $default = null) {
+  public function getFile($module, $file, $ext, $default = null) {
 
-    if($dirs = $this->{$type}()) {
-      // $extensions as array without dots
-      $extensions = array_map(function($ext) {
-        return ltrim($ext, '.');
-      }, (array)$extensions);
+    if($dirs = $this->{$module}()) {
+      // $ext as array without dots
+      $ext = array_map(function($e) {
+        return ltrim($e, '.');
+      }, (array)$ext);
 
       // add default location as possible source
-      if(!is_null($default)) {
-        array_unshift($dirs, $default);
-      }
+      if(!is_null($default)) array_unshift($dirs, $default);
 
       foreach($dirs as $dir) {
-        $root = f::resolve($dir . DS . $file, $extensions);
+        $root = f::resolve($dir . DS . $file, $ext);
         if($root) return $root;
       }
 
-      return $default . DS . $file . $extensions[0];
-
-    } else {
-      return null;
     }
+
+    return !is_null($default) ? $default . DS . $file . $ext[0] : null;
 
   }
 
-  public function allFiles($type, $default = null, $sort = false) {
+  public function allFiles($module, $default = null, $sort = false) {
     $files = array();
 
-    foreach($this->allRoots($type, $default) as $dir) {
+    foreach($this->allRoots($module, $default) as $dir) {
       foreach(dir::read($dir) as $file){
         $files[] = $file;
       }
     }
 
-    if($sort === true) {
-      sort($files);
-    }
+    if($sort === true) sort($files);
 
     return array_unique($files);
   }
 
-  public function allRoots($type, $default = null) {
-    $dirs = $this->{$type}();
+  public function allRoots($module, $default = null) {
+    $dirs = $this->{$module}();
     if(is_null($dirs)) $dirs = array();
     if($default) $dirs[] = $default;
     return $dirs;
