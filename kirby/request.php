201cf0e5b3144cf030c6c50cefba9434a8a1b89a
Bastian Allgeier <mail@bastianallgeier.com>
2016-08-16 16:51:21 +0200

    debuginfo for Kirby\Request objects

--- kirby/request.php
+++ kirby/request.php
@@ -38,4 +38,24 @@ class Request {
     }
   }
 
+  /**
+   * Improved var_dump() output
+   * 
+   * @return array
+   */
+  public function __debuginfo() {
+    return [
+      'url'     => $this->url(),
+      'method'  => $this->method(),
+      'referer' => $this->referer(),
+      'ip'      => $this->ip(),
+      'ajax'    => $this->ajax(),
+      'scheme'  => $this->scheme(),
+      'ssl'     => $this->ssl(),
+      'params'  => $this->params(),
+      'query'   => $this->query(),
+      'path'    => $this->path()
+    ];
+  }
+
 }
