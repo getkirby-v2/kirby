a1a64df8176fa87f9f3dd50b8b41d0fb55222f7c
Lukas Bestle <mail@lukasbestle.com>
2016-09-12 15:35:46 +0200

    Simpler Request\Params::__toString implementation

--- kirby/request/params.php
+++ kirby/request/params.php
@@ -8,15 +8,7 @@ use Url;
 class Params extends Obj {
 
   public function __toString() {
-
-    $params = array();
-
-    foreach((array)$this as $key => $value) {
-      $params[] = $key . url::paramSeparator() . $value;
-    }
-
-    return implode('/', $params);
-
+    return url::paramsToString($this->toArray());
   }
 
 }
