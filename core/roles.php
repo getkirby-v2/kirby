053e76faf1a1b7c8f5ba161bb0c22c61f49e39ad
Bastian Allgeier <mail@bastianallgeier.com>
2016-09-27 17:13:50 +0200

    The default editor role can no longer edit foreign avatars

--- core/roles.php
+++ core/roles.php
@@ -47,6 +47,9 @@ abstract class RolesAbstract extends Collection {
         'permissions' => [
           '*'                 => true,
           'panel.site.update' => false,
+          'panel.avatar.*'    => function() {
+            return $this->user()->is($this->target()->user());
+          },
           'panel.user.*'      => false,
           'panel.user.read'   => true,
           'panel.user.update' => function() {
