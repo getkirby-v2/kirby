d4508c04cd263072b54604ece464347814a63f5b
Bastian Allgeier <mail@bastianallgeier.com>
2017-08-02 13:24:51 +0200

    Update toolkit to 2.5.4 and bump version number

--- kirby.php
+++ kirby.php
@@ -10,7 +10,7 @@ use Kirby\Urls;
 
 class Kirby {
 
-  static public $version = '2.5.3';
+  static public $version = '2.5.4';
   static public $instance;
   static public $hooks = array();
   static public $triggered = array();
