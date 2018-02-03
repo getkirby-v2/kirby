646778e8a427c167e53179d418349b19c6ee4c69
Lukas Bestle <mail@lukasbestle.com>
2018-02-03 17:53:40 +0100

    $image->crop(): Make sure that height is defined

--- kirby/traits/image.php
+++ kirby/traits/image.php
@@ -76,9 +76,12 @@ trait Image {
    */
   public function crop($width, $height = null, $quality = null) {
 
-    $params = ['width' => $width, 'crop' => true];
+    $params = [
+      'width'  => $width,
+      'height' => ($height)? $height : $width,
+      'crop'   => true
+    ];
 
-    if($height)  $params['height']  = $height;
     if($quality) $params['quality'] = $quality;
 
     return $this->thumb($params);
