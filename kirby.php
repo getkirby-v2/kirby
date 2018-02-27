bdf5e0181b7f4a501b10811ddc512455f53047e2
Bastian Allgeier <mail@bastianallgeier.com>
2018-02-27 16:40:11 +0100

    Update version number

--- kirby.php
+++ kirby.php
@@ -10,7 +10,7 @@ use Kirby\Urls;
 
 class Kirby {
 
-  static public $version = '2.5.9 RC-2';
+  static public $version = '2.5.9';
   static public $instance;
   static public $hooks = array();
   static public $triggered = array();
