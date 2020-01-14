a7c90c6d330db800887918ea86cdb60e3bd86981
Bastian Allgeier <mail@bastianallgeier.com>
2020-01-14 10:01:12 +0100

    Update dependencies

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInitf5a857c9e4c52bed0abd021c7daaa7b8
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitf5a857c9e4c52bed0abd021c7daaa7b8', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
